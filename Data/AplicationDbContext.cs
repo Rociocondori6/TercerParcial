1676cb4251a4bf69f8d1e222c5d3e0f40e9a34ba
Rociocondori6 <condorirocio782@gmail.com>
2025-11-05 15:21:46 -0300

    Data condigurado

--- Data/AplicationDbContext.cs
+++ Data/AplicationDbContext.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
-namespace ClasesEjercicioExamen.Data
+namespace Clases.Data
 {
-    internal class AplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Server=LAPTOP-J7C6C098\\SQLEXPRESS;Database=Parcial3;Trusted_Connection=True;TrustServerCertificate=True;"
+
+            );
+        }
+
+
+
     }
 }
